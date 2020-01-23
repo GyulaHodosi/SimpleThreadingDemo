b76cd8126c3b134d26bf4846612810e5582ec524
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-01-23 18:54:01 +0100

    Implement Counting and Threading

--- SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimpleThreadingDemo
 {
@@ -10,6 +11,24 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread thread1 = new Thread(Counting);
+            Thread thread2 = new Thread(Counting);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.ReadKey();
+        }
+
+        public static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Count: { i + 1}");
+                Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}\n");
+                Thread.Sleep(10);
+            }
         }
     }
 }
